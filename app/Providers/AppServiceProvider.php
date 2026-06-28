d542d25404f3c76b029bea06363f0e741ab5f89b
Joanna Avalos <avalos22@users.noreply.github.com>
2026-06-27 19:36:40 -0600

    hot fix deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if (str_starts_with(config('app.url', ''), 'https://')) {
             URL::forceScheme('https');
         }
 
